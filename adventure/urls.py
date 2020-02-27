92687018f4a783f581e64a1c9bd4be417562d0e0
John <johnopana2016@gmail.com>
2020-02-27 10:03:29 +0300

    modified urls.py

--- adventure/urls.py
+++ adventure/urls.py
@@ -1,1 +1,16 @@
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views
+from django_registration.backends.one_step.views import RegistrationView
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('houses.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
+]
 
